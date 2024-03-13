ce7a03461bb5f8dc68267289d6366b0c283538d7
Prantor-Hawlader <prantor502@gmail.comm>
2024-03-13 20:10:31 +0600

    refactored message and conversation mechanism

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { sendMessage } from "../controllers/message.controllers.js";
+import {
+  getMessages,
+  sendMessage,
+} from "../controllers/message.controllers.js";
 
 const router = express.Router();
 
 router.post("/send/:id", protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessages);
 export default router;
